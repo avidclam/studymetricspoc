5280bd1eadc6993aaf48836b5395e4a68321c11f
Aleksandr Mikhailov <dev@avidclam.com>
2025-08-13 18:43:17 +0200

    Refactor export generator

--- studymetricspoc/serialize.py
+++ studymetricspoc/serialize.py
@@ -19,15 +19,36 @@ def serialize(data):
     return json.dumps(data, default=serializer, indent=2)
 
 def gen_export(results):
+    all_parameter_keys = set()
     for op in results:
-        data_item = {}
-        for k, v in op.items():
-            if k in ['metric', 'level', 'result']:
-                data_item[k] = v
-            elif k == 'parameter':
-                for pk, pv in v.items():
-                    data_item[pk] = pv
-            elif k == 'scope':
-                data_item['scope'] = v.get('name', 'Unknown')
-        yield data_item
-
+        all_parameter_keys.update(op.get('parameter', {}).keys())
+    for op in results:
+        if 'level' in op and 'metric' in op:
+            base_record = {
+                'scope': op['scope'].get('name', 'Unknown') if 'scope' in op else None,
+                'metric': op['metric'],
+                'level': op['level']
+            }
+            level = base_record['level']
+            for param_key in all_parameter_keys:
+                base_record[param_key] = op.get('parameter', {}).get(param_key)
+            df = op.get('result', {'error': 'Missing result'})
+            if isinstance(df, pl.DataFrame):
+                for record in df.to_dicts():
+                    record_dict = {}
+                    record_dict.update(base_record)
+                    record_dict.update({
+                        'level_value': record.get(level),
+                        'metric_value': record.get('value'),
+                        'error': None
+                    })
+                    yield record_dict
+            else:
+                record_dict = {}
+                record_dict.update(base_record)
+                record_dict.update({
+                    'level_value': None,
+                    'metric_value': None,
+                    'error': df.get('error', 'Unknown error')
+                })
+                yield record_dict
